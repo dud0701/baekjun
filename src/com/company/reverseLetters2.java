b65c97fd02a59661dbfeab2b3fae92f2196edde1
cye <cye@vivans.net>
2019-10-08 16:14:46 +0900

    boj_17413 단어 뒤집기2

---
+++ src/com/company/reverseLetters2.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Stack;
+
+public class reverseLetters2 {
+    public static void main(String[] args) throws IOException {
+
+        boolean tag = false;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> s = new Stack<>();
+
+
+        String sentence = br.readLine();
+
+        for(int i = 0; i < sentence.length();i++){
+            char letter = sentence.charAt(i);
+            if(letter == '<') {
+                tag = true;
+                print(s);
+                System.out.print(letter);
+            }else if(letter == '>'){
+                tag = false;
+                System.out.print(letter);
+
+            }else if(tag){
+                System.out.print(letter);
+            }else{
+                if(letter == ' '){
+                    print(s);
+                    System.out.print(" ");
+                }else{
+                    s.push(letter);
+                }
+            }
+        }
+
+        print(s);
+
+    }
+
+    public static void print(Stack<Character> s){
+        while(!s.empty()){
+            System.out.print(s.pop());
+        }
+    }
+}
